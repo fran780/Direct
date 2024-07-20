72afd2e0eaceb79f27d019ebab4dc37bf12432bf
fran780 <franciscofernandez365@yahoo.com>
2024-07-19 21:05:52 -0600

    Etapa 16
    - Creación de base de datos en Firebase Firestore.
    -Creación colecciones de usuarios.
    - Integración de las apis necesarias para utilizar Firestore.

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs{
+  //para autenticarse
   static FirebaseAuth auth = FirebaseAuth.instance;
+
+  //para tener acceso a la base de datos de firestore
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
