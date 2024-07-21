3f93cb84cd18f333cdd8a3687c86cfd0af8ea76f
fran780 <franciscofernandez365@yahoo.com>
2024-07-20 20:39:04 -0600

    Etapa 21
    - Importación de API para mostrar imágenes de red almacenadas en cache.
    - Actualización del diseño de la imagen de perfil de los chats, además de mostrar su respectiva imagen de perfil.

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:direct/models/chat_user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +26,17 @@ class _ChatUserCardState extends State<ChatUserCard> {
         onTap: () {},
         child: ListTile(
           // Imagen de perfil del usuario
-          leading: const CircleAvatar(child: Icon(CupertinoIcons.person)),
+          //leading: const CircleAvatar(child: Icon(CupertinoIcons.person)),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(mq.height * .3),
+            child: CachedNetworkImage(
+              width: mq.height * .055,
+              height: mq.height * .055,
+              imageUrl: widget.user.image,
+              errorWidget: (context, url, error) =>
+                  const CircleAvatar(child: Icon(CupertinoIcons.person)),
+            ),
+          ),
 
           // Nombre de usuario
           title: Text(widget.user.name),
@@ -34,12 +45,19 @@ class _ChatUserCardState extends State<ChatUserCard> {
           subtitle: Text(widget.user.about, maxLines: 1),
 
           // Tiempo del ultimo mensaje
-          trailing: Text(
+          trailing: Container(
+            width: 15,
+            height: 15,
+            decoration: BoxDecoration(
+                color: Colors.greenAccent.shade400,
+                borderRadius: BorderRadius.circular(10)),
+          ),
+          /* trailing: Text(
             '12:00 PM',
           style: TextStyle(color: Colors.black54),
+        ),*/
         ),
       ),
-    ),
     );
   }
 }
